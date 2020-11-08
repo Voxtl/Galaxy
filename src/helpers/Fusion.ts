91ecfef5762d1b7bf8cc955db94793daefc17e36
LukeHallett <luke@larval.co.uk>
2020-11-08 11:49:48 +0000

    Remore FusionAuth helper

--- src/helpers/Fusion.ts
+++
@@ -1,3 +0,0 @@
-const { FusionAuthClient } = require('@fusionauth/typescript-client');
-
-module.exports = new FusionAuthClient('4me336D6LP_MkMOjlTDjO-firMWuTuKWKsRvTge3dvQ', 'https://auth.voxtl.tv');
