238837370ca3dfb01d6ac58bafef24e89a4c8d31
VITALISED <william.castro30@gmail.com>
2021-02-07 14:14:51 +1100

    :/

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,8 +1,12 @@
+import { ChannelModel } from "./ChannelModel"
+import { ProfileModel } from "./ProfileModel"
 import { Model } from "sequelize"
+import type { HasOneGetAssociationMixin } from "sequelize"
 import type { GlobalRole, _UserAttributes } from "@voxtl/types"
 import type { v5 as uuid } from "uuid"
 
 export class UserModel extends Model<_UserAttributes> implements _UserAttributes {
+    // Data
     public id!: typeof uuid
     public username!: string
     public verified!: boolean
@@ -11,5 +15,10 @@ export class UserModel extends Model<_UserAttributes> implements _UserAttributes
     public profile_id!: typeof uuid
     public channel_id!: typeof uuid
 
+    public readonly updated_at!: Date
     public readonly created_at!: Date
+
+    // Methods
+    public getChannel!: HasOneGetAssociationMixin<ChannelModel>
+    public getProfile!: HasOneGetAssociationMixin<ProfileModel>
 }
